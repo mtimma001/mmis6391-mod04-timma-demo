bb8951fc25697e35fc893f7d556b17065bf768ae
Michele Timma <Timma28@gmail.com>
2024-10-16 20:34:01 +0300

    feat: Pet Project Integration

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,15 @@ from . import app
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/adoption')
+def adoption():
+    return render_template('adoption.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
